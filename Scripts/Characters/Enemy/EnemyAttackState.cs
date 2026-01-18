5ac0fa7b9ac3217fb4dbec7fa760d42139b5b4ba
Rose Tovar <rose@vanica4rize.com>
2026-01-18 13:43:52 -0500

    new work

--- Scripts/Characters/Enemy/EnemyAttackState.cs
+++ Scripts/Characters/Enemy/EnemyAttackState.cs
@@ -1,10 +1,22 @@
-using System;
+using System.Linq;
 using Godot;
 
 public partial class EnemyAttackState : EnemyState
 {
+    private Vector3 targetPos;
+
     protected override void EnterState()
     {
         characterNode.animationPlayer.Play(GameConstants.ANIM_ATTACK);
+
+        Node3D target = characterNode.attackAreaNode.GetOverlappingBodies().First();
+
+        targetPos = target.GlobalPosition;
+    }
+
+    private void PerformHit()
+    {
+        characterNode.ToggleHitBox(false);
+        characterNode.hitBox.GlobalPosition = targetPos;
     }
 }
